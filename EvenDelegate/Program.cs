999a42509a259cae2fff78583418f0fcf0184073
jegyvasarlas <88721560+jegyvasarlas@users.noreply.github.com>
2022-12-09 11:59:27 +0100

    initial3

---
+++ EvenDelegate/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EvenDelegate
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Alkalmazott jozsi = new Alkalmazott("Kiss József", 350000);
+            jozsi.FizetesemelesErtesites += FizetesemelesErtesites;
+            Alkalmazott bela = new Alkalmazott("Nagy Béla", 340000);
+            bela.FizetesemelesErtesites += FizetesemelesErtesites;
+            Alkalmazott sanyi = new Alkalmazott("Tóth Sándor", 480000);
+            Alkalmazott piros = new Alkalmazott("Kovács Piroska", 500000);
+
+            jozsi.Fizetes = 360000;
+            
+            bela.Fizetes = 400000;
+            
+        }
+        
+        private static void FizetesemelesErtesites(string uzenet)
+        {
+            Console.WriteLine(uzenet);
+        }
+    }
+}
---
+++ EvenDelegate/Properties/Alkalmazott.cs
@@ -0,0 +1,34 @@
+﻿namespace EvenDelegate
+{
+    public delegate void EventHandlerDelegate(string uzenet);
+    
+    class Alkalmazott
+    {
+        private string nev;
+        private int fizetes;
+
+        public int Fizetes
+        {
+            set
+            {
+                fizetes = value;
+                FizetesEmeles();
+            }
+        }
+        
+        public event EventHandlerDelegate FizetesemelesErtesites;
+        public Alkalmazott(string nev, int fizetes)
+        {
+            this.nev = nev;
+            this.fizetes= fizetes;
+        }
+        
+        public void FizetesEmeles()
+        {
+            if(FizetesemelesErtesites != null)
+            {
+                FizetesemelesErtesites("Fizetés emelés!!!!!!!");
+            }
+        }
+    }
+}
