5734685e3e3761ef1516f9bb07236304f5c14134
jegyvasarlas <88721560+jegyvasarlas@users.noreply.github.com>
2023-01-11 10:08:06 +0100

    b

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Bemenet = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pozitiv szam:";
+            // 
+            // Bemenet
+            // 
+            this.Bemenet.Location = new System.Drawing.Point(130, 25);
+            this.Bemenet.Name = "Bemenet";
+            this.Bemenet.Size = new System.Drawing.Size(166, 26);
+            this.Bemenet.TabIndex = 1;
+            this.Bemenet.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Gombnyomas);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(69, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Szamol";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Szamol);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(323, 121);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Bemenet);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Gombnyomas);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Bemenet;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Calc()
+        {
+            string bemenet = Bemenet.Text;
+            try
+            {
+                double szam = double.Parse(bemenet);
+                if (szam < 0)
+                {
+                    throw new Exception("Nem pozitiv a beirt szam!");
+                }
+                MessageBox.Show($"A beolvasott szam:  {szam} {Environment.NewLine}{Environment.NewLine}Negyzete: {szam * szam}{Environment.NewLine}Kobe: {szam * szam * szam}{Environment.NewLine}Negyzetgyoke: {Math.Sqrt(szam)}");
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Szamol(object sender, EventArgs e)
+        {
+            Calc();
+        }
+
+        private void Gombnyomas(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == (char)Keys.Enter)
+            {
+                Calc();
+            }
+        }
+    }
+}
